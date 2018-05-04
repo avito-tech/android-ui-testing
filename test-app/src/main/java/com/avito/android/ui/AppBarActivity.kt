4d868d93459ee50cd2f9c6817f21f5c5af0c4b5f
Dmitry Gryazin <dsgryazin@avito.ru>
2018-05-05 02:54:19 +0300

    Add appbar element with test

---
+++ test-app/src/main/java/com/avito/android/ui/AppBarActivity.kt
@@ -0,0 +1,25 @@
+package com.avito.android.ui
+
+import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+class AppBarActivity : AppCompatActivity() {
+
+    lateinit var appBarLayout: AppBarLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_bar)
+        appBarLayout = findViewById(R.id.appbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    fun setExpanded(isExpanded: Boolean) {
+        appBarLayout.handler.post {
+            appBarLayout.setExpanded(isExpanded)
+        }
+    }
+}
---
+++ ui-testing-core/src/main/java/com/avito/android/test/page_object/AppBarElement.kt
@@ -0,0 +1,50 @@
+package com.avito.android.test.page_object
+
+import android.support.design.widget.AppBarLayout
+import android.support.test.espresso.UiController
+import android.support.test.espresso.ViewAction
+import android.support.test.espresso.matcher.ViewMatchers
+import android.view.View
+import com.avito.android.test.InteractionContext
+import com.avito.android.test.SimpleInteractionContext
+import com.avito.android.test.action.Actions
+import com.avito.android.test.action.ActionsDriver
+import com.avito.android.test.action.ActionsImpl
+import org.hamcrest.Matcher
+
+class AppBarElement(interactionContext: InteractionContext) :
+    PageObjectElement(interactionContext) {
+
+    constructor(matcher: Matcher<View>) : this(SimpleInteractionContext(matcher))
+
+    override val actions: AppBarActions = AppBarActionsImpl(interactionContext)
+}
+
+interface AppBarActions : Actions {
+    fun collapse()
+    fun expand()
+}
+
+class AppBarActionsImpl(private val driver: ActionsDriver) : AppBarActions,
+    Actions by ActionsImpl(driver) {
+
+    override fun collapse() {
+        driver.perform(SetExpandedAction(isExpanded = false))
+    }
+
+    override fun expand() {
+        driver.perform(SetExpandedAction(isExpanded = true))
+    }
+}
+
+private class SetExpandedAction(private val isExpanded: Boolean) : ViewAction {
+
+    override fun getConstraints(): Matcher<View> =
+        ViewMatchers.isAssignableFrom(AppBarLayout::class.java)
+
+    override fun getDescription(): String = "Set expanded property of AppBarLayout"
+
+    override fun perform(uiController: UiController, view: View) {
+        (view as AppBarLayout).setExpanded(isExpanded)
+    }
+}

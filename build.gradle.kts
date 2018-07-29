b56c90f2bb1a4c78d92b74afdf1ad25188d77949
Dmitriy Voronin <dsvoronin@gmail.com>
2018-07-29 15:56:13 +0300

    update gradle to 4.9; slightly refactor scripts (#20)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryPlugin
 import com.android.builder.model.AndroidLibrary
 import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.BintrayPlugin
 import org.gradle.api.internal.plugins.DslObject
 import org.gradle.api.publish.maven.MavenPom
 import kotlin.properties.Delegates
@@ -18,9 +19,9 @@ buildscript {
 
     dependencies {
         classpath(kotlin("gradle-plugin", kotlinVersion))
-        classpath("com.android.tools.build:gradle:3.1.1")
+        classpath("com.android.tools.build:gradle:3.1.3")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.0")
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     }
 }
 
@@ -30,49 +31,52 @@ version = "0.2.1"
 val minSdk: String by project
 val targetSdk: String by project
 
-subprojects.forEach { subProject: Project ->
-    subProject.repositories {
+subprojects {
+    repositories {
         jcenter()
         google()
     }
 
-    subProject.group = rootProject.group
-    subProject.version = rootProject.version
+    group = rootProject.group
+    version = rootProject.version
 
-    subProject.plugins.withType(LibraryPlugin::class.java) {
+    plugins.withType(LibraryPlugin::class.java) {
 
-        subProject.apply {
-            plugin("com.github.dcendents.android-maven")
-            plugin("com.jfrog.bintray")
-        }
+        apply(plugin = "com.github.dcendents.android-maven")
+        apply<BintrayPlugin>()
 
-        val sourcesJarTask = subProject.tasks.create<Jar>("sourcesJar") {
+        val sourcesJarTask = tasks.create<Jar>("sourcesJar") {
             classifier = "sources"
-            from(subProject.extensions.getByType<BaseExtension>().sourceSets.getByName("main").java.srcDirs)
+            from(
+                this@subprojects.extensions.getByType(BaseExtension::class.java).sourceSets.getByName(
+                    "main"
+                ).java.srcDirs
+            )
         }
 
-        val installTask = subProject.tasks.getByName("install")
+        val installTask = tasks.getByName("install")
 
         (installTask as Upload).run {
-            DslObject(repositories).convention.getPlugin(MavenRepositoryHandlerConvention::class.java).mavenInstaller {
-                pom {
-                    project {
-                        packaging = "aar"
-                        groupId = rootProject.group.toString()
-                        artifactId = subProject.name
-                        version = rootProject.version.toString()
+            DslObject(repositories).convention.getPlugin(MavenRepositoryHandlerConvention::class.java)
+                .mavenInstaller {
+                    pom {
+                        project {
+                            packaging = "aar"
+                            groupId = rootProject.group.toString()
+                            artifactId = name
+                            version = rootProject.version.toString()
+                        }
                     }
                 }
-            }
         }
 
-        subProject.tasks.getByName("bintrayUpload").dependsOn(installTask)
+        tasks.getByName("bintrayUpload").dependsOn(installTask)
 
-        subProject.artifacts {
+        artifacts {
             add("archives", sourcesJarTask)
         }
 
-        subProject.extensions.getByType<BintrayExtension>().run {
+        configure<BintrayExtension> {
             user = System.getenv("BINTRAY_USER")
             key = System.getenv("BINTRAY_API_KEY")
 
@@ -100,13 +104,9 @@ subprojects.forEach { subProject: Project ->
     }
 }
 
-task<Wrapper>("wrapper") {
-    gradleVersion = "4.8.1"
+tasks.withType<Wrapper> {
+    gradleVersion = "4.9"
     distributionType = Wrapper.DistributionType.BIN
 }
 
 task<Delete>("clean") { delete("${rootProject.buildDir}") }
-
-inline fun <reified T> ExtensionContainer.getByType(): T = getByType(T::class.java)
-
-inline fun <reified T : Task> TaskContainer.create(name: String, noinline configure: T.() -> Unit): T = create(name, T::class.java, configure)

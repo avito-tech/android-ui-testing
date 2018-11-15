e9105b414a802d36e95ebe72a5dea0ce39685e24
Andrey Kovalev <avkovalev@avito.ru>
2018-11-15 13:35:14 +0300

    add toolbar checks with string matcher

--- test-app/src/androidTest/java/com/avito/android/ui/test/AppBarScreen.kt
+++ test-app/src/androidTest/java/com/avito/android/ui/test/AppBarScreen.kt
@@ -2,12 +2,15 @@ package com.avito.android.ui.test
 
 import android.support.test.espresso.matcher.ViewMatchers
 import com.avito.android.test.page_object.AppBarElement
+import com.avito.android.test.page_object.ToolbarElement
 import com.avito.android.test.page_object.ViewElement
 import com.avito.android.ui.R
 
 class AppBarScreen {
 
     val appBar = AppBarElement(ViewMatchers.withId(R.id.appbar))
+
+    val toolbar = ToolbarElement(ViewMatchers.withId(R.id.toolbar))
 
     val testView = ViewElement(ViewMatchers.withId(R.id.view_in_collapsing_toolbar))
 }
--- test-app/src/androidTest/java/com/avito/android/ui/test/AppBarTest.kt
+++ test-app/src/androidTest/java/com/avito/android/ui/test/AppBarTest.kt
@@ -1,6 +1,8 @@
 package com.avito.android.ui.test
 
 import com.avito.android.ui.AppBarActivity
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.startsWith
 import org.junit.Rule
 import org.junit.Test
 
@@ -16,6 +18,8 @@ class AppBarTest {
         rule.activity.setExpanded(true)
 
         Screen.appBarScreen.appBar.actions.collapse()
+        Screen.appBarScreen.toolbar.checks.withTitle(startsWith("Tit"))
+        Screen.appBarScreen.toolbar.checks.withSubtitle(`is`("Subtitle"))
         Screen.appBarScreen.testView.checks.isNotCompletelyDisplayed()
     }
 

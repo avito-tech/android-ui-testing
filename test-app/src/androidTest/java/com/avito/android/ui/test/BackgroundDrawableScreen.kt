a0f5a696e4f5c2b576f9eba5fc4dfe431838e8df
Andrey Kovalev <avkovalev@avito.ru>
2018-11-23 09:28:37 +0300

    refactor webview and elements

--- test-app/src/androidTest/java/com/avito/android/ui/test/BackgroundDrawableScreen.kt
+++ test-app/src/androidTest/java/com/avito/android/ui/test/BackgroundDrawableScreen.kt
@@ -4,9 +4,7 @@ import android.support.test.espresso.matcher.ViewMatchers
 import com.avito.android.test.page_object.ViewElement
 import com.avito.android.ui.R
 
-class BackgroundDrawableScreen  {
-
+class BackgroundDrawableScreen {
     val viewWithBackgroundRedColor = ViewElement(ViewMatchers.withId(R.id.background_view_color))
-
     val viewWithBackgroundCheckIcon = ViewElement(ViewMatchers.withId(R.id.background_view_image))
 }
---
+++ ui-testing-core/src/main/java/com/avito/android/test/page_object/WebView.kt
@@ -0,0 +1,50 @@
+package com.avito.android.test.page_object
+
+import android.support.test.espresso.web.assertion.WebViewAssertions.webMatches
+import android.support.test.espresso.web.model.Atom
+import android.support.test.espresso.web.model.Atoms.getCurrentUrl
+import android.support.test.espresso.web.model.ElementReference
+import android.support.test.espresso.web.sugar.Web
+import android.view.View
+import com.avito.android.test.action.WebElementActions
+import com.avito.android.test.checks.WebElementChecks
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.containsString
+import java.util.concurrent.TimeUnit
+
+open class WebView(private val webViewMatcher: Matcher<View>) : PageObject {
+
+    private val interaction: Web.WebInteraction<Void>
+        get() = Web.onWebView(webViewMatcher)
+
+    val checks = WebViewChecksImpl()
+
+    fun withElement(elementMatcher: Atom<ElementReference>) = WebViewElement(elementMatcher)
+
+    interface WebViewChecks {
+
+        fun withUrl(uri: String)
+    }
+
+    inner class WebViewChecksImpl : WebViewChecks {
+
+        override fun withUrl(uri: String) {
+            interaction.check(webMatches(getCurrentUrl(), containsString(uri)))
+        }
+    }
+
+    inner class WebViewElement(elementMatcher: Atom<ElementReference>) : PageObject {
+
+        val actions = WebElementActions(
+            interaction.withTimeout(5, TimeUnit.SECONDS).withElement(
+                elementMatcher
+            )
+        )
+
+        val checks = WebElementChecks(
+            interaction.withTimeout(5, TimeUnit.SECONDS).withElement(
+                elementMatcher
+            )
+        )
+    }
+}

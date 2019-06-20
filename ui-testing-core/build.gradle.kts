e35cf76e5a1b8a5583f9eb435404362ddfd7454f
dmerkurev <dmerkurev@avito.ru>
2019-06-20 16:58:10 +0300

    migrate to espresso 3.0.2

--- ui-testing-core/build.gradle.kts
+++ ui-testing-core/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     api("com.android.support.test.espresso:espresso-web:$espressoVersion")
     api("com.android.support.test.espresso:espresso-intents:$espressoVersion")
     api("com.android.support.test.uiautomator:uiautomator-v18:2.1.3")
-    api("com.forkingcode.espresso.contrib:espresso-descendant-actions:1.2.0")
+    api("com.forkingcode.espresso.contrib:espresso-descendant-actions:1.3.0")
     api("com.android.support:appcompat-v7:$supportVersion")
     api("com.android.support:recyclerview-v7:$supportVersion")
     api("com.android.support:design:$supportVersion")
